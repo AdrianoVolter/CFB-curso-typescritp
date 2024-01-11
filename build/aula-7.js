b69f0711d6cb6b4b429316aa6ca423c70edbad94
adrianovolter <adriano.j.volter@hotmail.com>
2024-01-11 17:27:26 -0300

    aula 7 typescript

---
+++ build/aula-7.js
@@ -0,0 +1,3 @@
+"use strict";
+let coisas = ['TS', 10, true];
+console.log(coisas[2]);
---
+++ src/aula-7.ts
@@ -0,0 +1,7 @@
+let coisas: readonly[string, number, boolean] = ['TS', 10, true];
+
+//coisas[0] = 'TypeScript';
+
+//coisas.push(2);
+
+console.log(coisas[2]);
