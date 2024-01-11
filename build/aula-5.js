2f008c4fbe7d4354502f28decd91a84977a4a745
adrianovolter <adriano.j.volter@hotmail.com>
2024-01-10 22:34:52 -0300

    aula 5 typescript

---
+++ build/aula-5.js
@@ -0,0 +1,7 @@
+"use strict";
+let cursos = ["javascript", "typescript", "c++"];
+let valores = [10, 20, 2023];
+cursos.push("java");
+valores.push(500);
+console.log(cursos);
+console.log(valores);
---
+++ src/aula-5.ts
@@ -0,0 +1,9 @@
+let cursos:string[] = [ "javascript", "typescript", "c++"]; 
+// array do typo string
+let valores = [10, 20, 2023]
+
+cursos.push("java");
+valores.push(500)
+
+console.log(cursos)
+console.log(valores)
