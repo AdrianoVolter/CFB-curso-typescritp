d78fcf294488ad69c5e31d18e6c2dcbaf103fbb3
adrianovolter <adriano.j.volter@hotmail.com>
2024-01-12 10:24:52 -0300

    aula 10 typescript

---
+++ build/aula-10.js
@@ -0,0 +1,9 @@
+"use strict";
+let lnome;
+lnome = null;
+console.log(lnome);
+let lnome2;
+console.log(lnome2);
+let vnome3 = 12;
+let vn = vnome3;
+console.log(vn);
---
+++ src/aula-10.ts
@@ -0,0 +1,16 @@
+// null undefined unknown
+
+let lnome:string|null ;
+
+lnome = null
+
+console.log(lnome)
+
+let lnome2;
+ console.log(lnome2)
+
+
+
+let vnome3:unknown=12; //unknown é um tipo de variavel que pode ser qualquer coisa
+let vn:any=vnome3
+console.log(vn) //any é um tipo de variavel que pode ser qualquer coisa
