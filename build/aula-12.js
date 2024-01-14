519ebe562b7026c315d6a1a6276a89e6d2365323
adrianovolter <adriano.j.volter@hotmail.com>
2024-01-14 10:14:04 -0300

    aula 12 typescript

---
+++ build/aula-12.js
@@ -0,0 +1,9 @@
+"use strict";
+function somar(num1, num2) {
+    return num1 + num2;
+}
+console.log(somar(2, 2));
+function mostrarMensagem() {
+    console.log('Olá mundo!');
+}
+mostrarMensagem();
---
+++ src/aula-12.ts
@@ -0,0 +1,16 @@
+// funções em typescript
+
+function somar(num1: number, num2: number): number {
+    return num1 + num2;
+ }    
+
+ console.log(somar(2, 2));
+
+
+// função com retorno void
+
+function mostrarMensagem(): void {
+    console.log('Olá mundo!');
+}
+
+mostrarMensagem();
