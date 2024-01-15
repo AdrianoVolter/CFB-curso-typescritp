56bf2fa8ad90c88479664b14c190c390be82e89b
adrianovolter <adriano.j.volter@hotmail.com>
2024-01-14 22:40:05 -0300

    aula 12 typescript/ 2

--- src/aula-12.ts
+++ src/aula-12.ts
@@ -9,8 +9,16 @@ function somar(num1: number, num2: number): number {
 
 // função com retorno void
 
-function mostrarMensagem(): void {
+function mostrarMensagem(): void { // void não retorna nada
     console.log('Olá mundo!');
 }
 
 mostrarMensagem();
+
+
+function logar(user:string, pass:string): void {
+    console.log(`Usuário: ${user}`);
+    console.log(`Senha: ${pass}`)
+}
+
+logar('admin', '123');
