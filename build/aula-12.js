3db7c9852df326d644168c0ec57256f4132b8887
adrianovolter <adriano.j.volter@hotmail.com>
2024-01-14 22:39:23 -0300

    aula 12 typescript/ 2

--- build/aula-12.js
+++ build/aula-12.js
@@ -7,3 +7,8 @@ function mostrarMensagem() {
     console.log('Olá mundo!');
 }
 mostrarMensagem();
+function logar(user, pass) {
+    console.log(`Usuário: ${user}`);
+    console.log(`Senha: ${pass}`);
+}
+logar('admin', '123');
