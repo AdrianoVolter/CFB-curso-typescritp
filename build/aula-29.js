1116d77c5c87c85fbb766ed0cf51c34d4673d4ab
adrianovolter <adriano.j.volter@hotmail.com>
2024-01-22 09:53:15 -0300

    aula 29 typescrip/ generics

--- build/aula-29.js
+++ build/aula-29.js
@@ -4,3 +4,13 @@ function f_teste(v, r) {
 }
 console.log(f_teste(10, "d"));
 console.log(f_teste("teste", 12));
+class C_teste {
+    valor;
+    constructor(valor) {
+        this.valor = valor;
+    }
+}
+const c_teste1 = new C_teste(10);
+const c_teste2 = new C_teste("teste");
+console.log(c_teste1.valor);
+console.log(c_teste2.valor);
--- src/aula-29.ts
+++ src/aula-29.ts
@@ -7,3 +7,16 @@ function f_teste<T, U>(v:T , r:U):U{ //T e U são tipos genéricos
 
 console.log(f_teste<number, string>(10, "d"));   //T e U são tipos genéricos
 console.log(f_teste<string, number>("teste", 12));  //T e U são tipos genéricos
+
+class C_teste<T>{
+    public valor:T;
+    constructor(valor:T){
+        this.valor=valor
+    }
+}
+
+const c_teste1 = new C_teste<number>(10);
+const c_teste2 = new C_teste<string>("teste");
+
+console.log(c_teste1.valor);
+console.log(c_teste2.valor);
