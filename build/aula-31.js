547837fba0fcf866f71cf7a77fea67906e683454
adrianovolter <adriano.j.volter@hotmail.com>
2024-01-22 17:35:33 -0300

    aula 31 typescrip/modulos

---
+++ build/aula-31.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Classes_1 = require("./Classes");
+const p1 = new Classes_1.Pessoa('John', 1.88);
+const o1 = new Classes_1.Objeto('Mesa');
+console.log(p1);
+console.log(o1);
--- src/aula-30.ts
+++ src/aula-30.ts
@@ -1,8 +1,8 @@
-import { Pessoa,Objeto }from './Classes'
+// import { Pessoa,Objeto }from './Classes'
 
 
-const p1 = new Pessoa('John', 1.88);
-const o1 = new Objeto('Mesa');
+// const p1 = new Pessoa('John', 1.88);
+// const o1 = new Objeto('Mesa');
 
-console.log(p1);
-console.log(o1);
+// console.log(p1);
+// console.log(o1);
